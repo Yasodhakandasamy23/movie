fdd6c44747d06490de63b655a637a2729e8ef54e
yasodhakandasamy23@gmail.com <yasodhakandasamy23@gmail.com>
2022-08-30 08:42:21 -0700

    movie

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,14 @@
 import { useState } from "react";
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
+
+
+
+
+
 
 
 export function Counter() {
@@ -7,10 +17,19 @@ export function Counter() {
   const [dislike, setDislike] = useState(0);
   return (
     <div className="container">
-      <button className="button-like" onClick={() => setlike(like + 1)}>👍{like}</button>
-      <button className="button-dislike" onClick={() => setDislike(dislike + 1)}>👎{dislike}</button>
-
-    </div>
+    
+    <IconButton  aria-label="Like"  onClick={() => setlike(like + 1)}  color="primary" > 
+    <Badge badgeContent={like} color="primary">
+      👍
+    </Badge>
+    </IconButton>
+  <IconButton  aria-label="DisLike"  onClick={() => setDislike(dislike + 1)} color="secondary" > 
+  <Badge badgeContent={dislike} color="error">
+  👎
+  </Badge>
+  </IconButton>
+  
+      </div>
   );
 }
 export default Counter;
